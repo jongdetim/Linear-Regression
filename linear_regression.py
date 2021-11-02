33a21c9fe9f3bdb26d31b66242e2d1ed668b9a45
tim <timdedenker@gmail.com>
2021-11-02 23:53:06 +0100

    gradient descent uses normalized x values. the plot transforms the values back to the unscaled original values. finally the thetas are converted abck to unscaled original values

--- linear_regression.py
+++ linear_regression.py
@@ -1,5 +1,4 @@
-
-import statistics as stats
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -8,23 +7,50 @@ def predict(x, m, c):
 
 
 def loss(y, x, m, c):
-    return stats.mean([(value - predict(x[i], m, c)) ** 2 for i, value in enumerate(y)])
+    return np.mean([(value - predict(x[i], m, c)) ** 2 for i, value in enumerate(y)])
 
 
-def update_plot(y_pred, i, line):
-    line.set_ydata([min(y_pred), max(y_pred)])
+def standardize_data(x_orig, y_orig):
+    x = (x_orig - np.mean(x_orig)) / np.std(x_orig)
+    # y = (y_orig - np.mean(y_orig)) / np.std(y_orig)
+    return x, y_orig
+
+
+# def destandardize_data(data, x_orig, y_orig):
+#     x = (x * np.std(x_orig)) + np.mean(x_orig)
+#     y = (y * np.std(y_orig)) + np.mean(y_orig)
+#     return x, y
+
+# def get_thetas_denormalized(x, y, m, c):
+#     standardized_pred = predict(x, m, c)
+#     pred = destandardize_data(standardized_pred)
+#     x, y = destandardize_data(x, y, x_orig, y_orig)
+
+def update_plot(y_pred, i, line, x_orig, m, c):
+    m_old = m
+    m = m / np.std(x_orig)
+    c = c - (m_old * np.mean(x_orig)) / np.std(x_orig)
+    y_pred = predict(x_orig, m, c)
+    if m < 0:
+        line.set_ydata([max(y_pred), min(y_pred)])
+        # line.set_xdata([max(x), min(x)])
+    else:
+        line.set_ydata([min(y_pred), max(y_pred)])
+        # line.set_xdata([min(x), max(x)])
     plt.draw()
     plt.pause(0.25 / (i + 1))
 
 
-def gradient_descent(epochs, l, x, y, m, c, draw_plot, line):
+def gradient_descent(epochs, l, x, y, m, c, draw_plot, line, x_orig):
+    n = float(len(x))
     for i in range(epochs):
-        c_new = l * stats.mean([predict(x[i], m, c) - value for i, value in enumerate(y)])
-        m_new = l * stats.mean([(predict(x[i], m, c) - value) * x[i] for i, value in enumerate(y)])
+        c_new = l * (1 / n) * sum([predict(x[i], m, c) - value for i, value in enumerate(y)])
+        m_new = l * (1 / n) * sum([(predict(x[i], m, c) - value) * x[i] for i, value in enumerate(y)])
         m, c = m - m_new, c - c_new
         y_pred = predict(x, m, c)
+        print(m, c)
         if draw_plot:
-            update_plot(y_pred, i, line)
+            update_plot(y_pred, i, line, x_orig, m, c)
     return (m, c)
 
 
@@ -37,5 +63,5 @@ def gradient_descent_partial_deriv(epochs, l, x, y, m, c, draw_plot, line):
         m = m - l * deriv_m
         c = c - l * deriv_c
         if draw_plot:
-            update_plot(y_pred, i, line)
+            update_plot(y_pred, i, line, x, m)
     return (m, c)
--- train_model.py
+++ train_model.py
@@ -6,11 +6,21 @@ import numpy as np
 
 if __name__ == "__main__":
 
-    x = np.array([1, 2, 3, 4, 3, 1, 6, 8, 4, 2])
-    y = np.array([6, 12, 18, 54, 32, 1, 23, 2, 6, 3])
-    m, c = 1, 0
-    l = 0.05
-    epochs = 100
+    try:
+        data = np.genfromtxt("data.csv", delimiter=',', dtype=int)
+        x_orig = data[1:,0]
+        y_orig = data[1:,1]
+    except:
+        print("no datafile present. please provide data as 'data.csv' in this directory")
+        exit(1)
+
+    x, y = standardize_data(x_orig, y_orig)
+
+    print(x, y)
+
+    m, c = 0.0, np.mean(y)
+    l = 0.025
+    epochs = 2000
     line = None
     draw_plot = False
     if len(sys.argv) > 1 and sys.argv[1] == "-v":
@@ -18,16 +28,22 @@ if __name__ == "__main__":
 
     if draw_plot:
         plt.ion()
-        plt.scatter(x, y)
-        y_pred = predict(x, m, c)
-        (line,) = plt.plot([min(x), max(x)], [min(y_pred), max(y_pred)], color="red")
-        line.axes.set_xlim(0, max(x) + 1)
-        line.axes.set_ylim(0, max(y) + 1)
+        plt.scatter(x_orig, y)
+        y_pred = predict(x_orig, m, c)
+        (line,) = plt.plot([min(x_orig), max(x_orig)], [min(y_pred), max(y_pred)], color="red")
+        line.axes.set_xlim(min(x_orig) - (max(x_orig) * 0.05), max(x_orig) * 1.05)
+        line.axes.set_ylim(min(y) - (max(y) * 0.05), max(y) * 1.05)
         plt.draw()
+        plt.pause(0.2)
 
-    m, c = gradient_descent(epochs, l, x, y, m, c, draw_plot, line)
+    m, c = gradient_descent(epochs, l, x, y, m, c, draw_plot, line, x_orig)
+    # m, c = get_thetas_denormalized(x, y, m, c)
+    m_old = m
+    m = m / np.std(x_orig)
+    c = c - (m_old * np.mean(x_orig)) / np.std(x_orig)
+
     print("m: ", m, "\nc: ", c)
-    print("loss: ", loss(y, x, m, c))
+    print("loss: ", loss(y_orig, x_orig, m, c))
     with open("thetas.txt", "w+") as file:
         file.write(str(m) + "\n" + str(c))
 
