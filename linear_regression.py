4968944d8632ebe921bf61daae2639b552f0bc62
Tim De jong <tide-jon@f1r2s6.codam.nl>
2021-11-15 18:18:37 +0100

    refactored into linear_regression class and functions

--- linear_regression.py
+++ linear_regression.py
@@ -2,66 +2,76 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def predict(x, m, c):
-    return m * x + c
+class linear_regression:
 
+    def __init__(self, m=0.0, c=0.0):
+        self.m = m
+        self.c = c
+        self.x_orig = None
+        self.plt = None
 
-def loss(y, x, m, c):
-    return np.mean([(value - predict(x[i], m, c)) ** 2 for i, value in enumerate(y)])
+    def predict(self, x, m, c):
+        return m * x + c
 
+    def loss(self, y, x, m, c):
+        return np.mean([(value - self.predict(x[i], m, c)) ** 2 for i, value in enumerate(y)])
 
-def standardize_data(x_orig, y_orig):
-    x = (x_orig - np.mean(x_orig)) / np.std(x_orig)
-    # y = (y_orig - np.mean(y_orig)) / np.std(y_orig)
-    return x, y_orig
+    def standardize_data(self, x_orig):
+        if self.x_orig is None:
+            self.x_orig = x_orig
+        x_standard = (self.x_orig - np.mean(self.x_orig)) / np.std(self.x_orig)
+        return x_standard
 
+    def get_destandardized_thetas(self):
+        if self.x_orig is None:
+            print("no original data known. please use model.fit() first")
+            return
+        c_destandard = self.c - (self.m * np.mean(self.x_orig)) / np.std(self.x_orig)
+        m_destandard = self.m / np.std(self.x_orig)
+        return m_destandard, c_destandard
 
-# def destandardize_data(data, x_orig, y_orig):
-#     x = (x * np.std(x_orig)) + np.mean(x_orig)
-#     y = (y * np.std(y_orig)) + np.mean(y_orig)
-#     return x, y
+    def update_plot(self, y_pred, i, line):
+        m_destandard, c_destandard = self.get_destandardized_thetas()
+        y_pred = self.predict(self.x_orig, m_destandard, c_destandard)
+        if self.m < 0:
+            line.set_ydata([max(y_pred), min(y_pred)])
+        else:
+            line.set_ydata([min(y_pred), max(y_pred)])
+        self.plt.draw()
+        self.plt.pause(0.25 / (i + 1))
 
-# def get_thetas_denormalized(x, y, m, c):
-#     standardized_pred = predict(x, m, c)
-#     pred = destandardize_data(standardized_pred)
-#     x, y = destandardize_data(x, y, x_orig, y_orig)
+    def gradient_descent(self, epochs, l, x, y, draw_plot, line):
+        n = float(len(x))
+        for i in range(epochs):
+            c_new = l * (1 / n) * sum([self.predict(x[i], self.m, self.c) - value for i, value in enumerate(y)])
+            m_new = l * (1 / n) * sum([(self.predict(x[i], self.m, self.c) - value) * x[i] for i, value in enumerate(y)])
+            self.m, self.c = self.m - m_new, self.c - c_new
+            y_pred = self.predict(x, self.m, self.c)
+            if draw_plot:
+                self.update_plot(y_pred, i, line)
+        return (self.m, self.c)
 
-def update_plot(y_pred, i, line, x_orig, m, c):
-    m_old = m
-    m = m / np.std(x_orig)
-    c = c - (m_old * np.mean(x_orig)) / np.std(x_orig)
-    y_pred = predict(x_orig, m, c)
-    if m < 0:
-        line.set_ydata([max(y_pred), min(y_pred)])
-        # line.set_xdata([max(x), min(x)])
-    else:
-        line.set_ydata([min(y_pred), max(y_pred)])
-        # line.set_xdata([min(x), max(x)])
-    plt.draw()
-    plt.pause(0.25 / (i + 1))
+    def gradient_descent_partial_deriv(self, epochs, l, x, y, draw_plot, line):
+        n = float(len(x))
+        for i in range(epochs):
+            y_pred = self.predict(x, self.m, self.c)
+            deriv_m = (-2 / n) * sum(x * (y - y_pred))
+            deriv_c = (-2 / n) * sum(y - y_pred)
+            m = m - l * deriv_m
+            c = c - l * deriv_c
+            if draw_plot:
+                self.update_plot(y_pred, i, line)
+        return (m, c)
 
-
-def gradient_descent(epochs, l, x, y, m, c, draw_plot, line, x_orig):
-    n = float(len(x))
-    for i in range(epochs):
-        c_new = l * (1 / n) * sum([predict(x[i], m, c) - value for i, value in enumerate(y)])
-        m_new = l * (1 / n) * sum([(predict(x[i], m, c) - value) * x[i] for i, value in enumerate(y)])
-        m, c = m - m_new, c - c_new
-        y_pred = predict(x, m, c)
-        print(m, c)
+    def fit(self, x, y, epochs=1000, l=0.025, draw_plot=False, line=None, plt=None):
+        self.plt = plt
         if draw_plot:
-            update_plot(y_pred, i, line, x_orig, m, c)
-    return (m, c)
-
-
-def gradient_descent_partial_deriv(epochs, l, x, y, m, c, draw_plot, line):
-    n = float(len(x))
-    for i in range(epochs):
-        y_pred = predict(x, m, c)
-        deriv_m = (-2 / n) * sum(x * (y - y_pred))
-        deriv_c = (-2 / n) * sum(y - y_pred)
-        m = m - l * deriv_m
-        c = c - l * deriv_c
-        if draw_plot:
-            update_plot(y_pred, i, line, x, m)
-    return (m, c)
+            plt.ion()
+            plt.scatter(self.x_orig, y)
+            y_pred = self.predict(self.x_orig, self.m, self.c)
+            (line,) = plt.plot([min(self.x_orig), max(self.x_orig)], [min(y_pred), max(y_pred)], color="red")
+            line.axes.set_xlim(min(self.x_orig) - (max(self.x_orig) * 0.05), max(self.x_orig) * 1.05)
+            line.axes.set_ylim(min(y) - (max(y) * 0.05), max(y) * 1.05)
+            plt.draw()
+            plt.pause(0.01)
+        self.gradient_descent(epochs, l, x, y, draw_plot, line)
--- train_model.py
+++ train_model.py
@@ -1,52 +1,50 @@
 #!/usr/bin/python3
 
-from linear_regression import *
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
+import argparse
 
-if __name__ == "__main__":
+from linear_regression import linear_regression
 
+
+def read_file():
     try:
         data = np.genfromtxt("data.csv", delimiter=',', dtype=int)
-        x_orig = data[1:,0]
-        y_orig = data[1:,1]
+        return data[1:, 0], data[1:, 1]
     except:
-        print("no datafile present. please provide data as 'data.csv' in this directory")
+        print("no datafile present. please provide data as 'data.csv' with 2 columns and headers in this directory")
         exit(1)
 
-    x, y = standardize_data(x_orig, y_orig)
 
-    print(x, y)
+def parse_args():
+    parser = argparse.ArgumentParser(description="train a linear regression model using gradient descent on a provided dataset")
+    parser.add_argument("--visualize", "-v", help="find a path as fast as possible, not guaranteed to be shortest solution", action='store_true')
+    args = parser.parse_args()
+    return args
 
-    m, c = 0.0, np.mean(y)
+
+def main():
     l = 0.025
-    epochs = 2000
+    epochs = 1000
     line = None
-    draw_plot = False
-    if len(sys.argv) > 1 and sys.argv[1] == "-v":
-        draw_plot = True
+    args = parse_args()
+    x_orig, y = read_file()
 
-    if draw_plot:
-        plt.ion()
-        plt.scatter(x_orig, y)
-        y_pred = predict(x_orig, m, c)
-        (line,) = plt.plot([min(x_orig), max(x_orig)], [min(y_pred), max(y_pred)], color="red")
-        line.axes.set_xlim(min(x_orig) - (max(x_orig) * 0.05), max(x_orig) * 1.05)
-        line.axes.set_ylim(min(y) - (max(y) * 0.05), max(y) * 1.05)
-        plt.draw()
-        plt.pause(0.2)
-
-    m, c = gradient_descent(epochs, l, x, y, m, c, draw_plot, line, x_orig)
-    # m, c = get_thetas_denormalized(x, y, m, c)
-    m_old = m
-    m = m / np.std(x_orig)
-    c = c - (m_old * np.mean(x_orig)) / np.std(x_orig)
+    model = linear_regression()
+    x = model.standardize_data(x_orig)
+    model.fit(x, y, epochs, l, args.visualize, line, plt)
+    m, c = model.get_destandardized_thetas()
 
     print("m: ", m, "\nc: ", c)
-    print("loss: ", loss(y_orig, x_orig, m, c))
+    print("loss: ", model.loss(y, x_orig, m, c))
     with open("thetas.txt", "w+") as file:
         file.write(str(m) + "\n" + str(c))
 
-    if draw_plot:
+    if args.visualize:
         plt.ioff()
         plt.show()
+
+
+if __name__ == "__main__":
+    main()
